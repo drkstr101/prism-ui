aa91bc83e766869dcf7c2ab1b4227ba7db937d72
Aaron Miller <amiller@aps.org>
2024-11-28 13:44:55 +0000

    feat: add tooling config to examples/hello

---
+++ examples/hello/eslint.config.js
@@ -0,0 +1,15 @@
+const nx = require('@nx/eslint-plugin');
+const baseConfig = require('../../eslint.config.js');
+
+module.exports = [
+  ...baseConfig,
+  ...nx.configs['flat/react'],
+  {
+    ignores: ['app/assets/builds'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+];
---
+++ examples/hello/jest.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from 'jest';
+
+export default {
+  displayName: 'hello',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/examples/hello',
+} satisfies Config;
