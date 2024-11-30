68d11cf2555480208b2259a3afcc16ba6b5813a7
Aaron R Miller <amiller@aps.org>
2024-11-29 21:57:10 -0800

    chore: update gitpod dev env (#3)
    * feat: add postgres to gitpod dev env
    
    * chore: install truffleruby/rails in gitpod env
    
    * chore: update gitpod env
    
    * chore: update default ruby in gitpod env
    
    * feat: add rails 8 example app
    
    * feat: add tooling config to examples/hello
    
    * chore: cleanup typescript config

---
+++ examples/hello/tailwind.config.js
@@ -0,0 +1,15 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js',
+  ],
+  presets: [require('../../tailwind-workspace-presets.js')],
+};
