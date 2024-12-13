1a95aa53b5b6b26fd767666d26a8a6053c536986
Aaron Miller <amiller@aps.org>
2024-12-13 21:04:45 +0000

    feat: add tailwind-syntax template

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,10 +1,15 @@
 //@ts-check
-import { composePlugins, withNx } from '@nx/next';
+
+//@ts-ignore
+import withMarkdoc from '@markdoc/next.js';
+import { withNx } from '@nx/next';
+import withSearch from './src/markdoc/search.mjs';
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
@@ -12,9 +17,9 @@ const nextConfig = {
   },
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
+// const plugins = [
+//   // Add more Next.js plugins to this list if needed.
+//   withNx,
+// ];
 
-export default composePlugins(...plugins)(nextConfig);
+export default withNx(withSearch(withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig)));
