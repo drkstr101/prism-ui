55de6c440a1b6a49abd8dfad2a4bd07994b1ef23
Aaron Miller <amiller@aps.org>
2024-12-13 20:32:30 +0000

    chore: switch to ems module config format

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,8 +1,8 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const { join } = require('path');
+import { createGlobPatternsForDependencies } from '@nx/react/tailwind';
+import { join } from 'path';
+import { type Config } from 'tailwindcss';
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'),
     ...createGlobPatternsForDependencies(__dirname),
@@ -11,4 +11,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
