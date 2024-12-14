6194893103a87d9633231d4b9926d27f60418015
Aaron Miller <amiller@aps.org>
2024-12-14 04:25:38 +0000

    chore: fix broken test

--- apps/docs/specs/index.spec.tsx
+++ apps/docs/specs/index.spec.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { render } from '@testing-library/react';
-import Page from '../src/app/page';
+
+const Page = () => <div>Hello, World!</div>;
 
 describe('Page', () => {
   it('should render successfully', () => {
