ace5cd04cae19029222df88aca1defe1920bae2b
Aaron Miller <amiller@aps.org>
2024-12-20 01:30:08 +0000

    chore: update typography stories

--- apps/docs/stories/theme/typography/typography.composition.tsx
+++ apps/docs/stories/theme/typography/typography.composition.tsx
@@ -1,19 +1,62 @@
-import { ReactNode } from 'react';
+import { HtmlHTMLAttributes } from 'react';
 
-export interface CompositionProps {
-  children?: ReactNode;
-}
+export type Props = HtmlHTMLAttributes<HTMLDivElement>;
 
-export function TypographyComposition() {
-  return (
-    <section className="typography">
-      <label className="detail">Detail</label>
-      <h1 className="heading">Phasellus maximus venenatis suscipit</h1>
-      <p className="body">
+export const TypographyComposition = (props: Props) => (
+    <section className="prose font-sans" {...props}>
+      <h1>Phasellus maximus venenatis suscipit</h1>
+      <p>
         Maecenas a dolor sed massa dignissim molestie. Sed nec hendrerit purus, sed cursus nisi.
         Vivamus sit amet tortor dapibus, lacinia tortor eget, efficitur sem.{' '}
       </p>
-      <div className="code">{"console.log('Hello, World!')"}</div>
+      <label>Detail</label>
+      <em>
+        <code>{"console.log('Hello, World!')"}</code>
+      </em>
     </section>
   );
-}
+
+export const HeadingComposition = (props: Props) => (
+    <div {...props}>
+      <div className="heading heading--3xl">Heading (3xl)</div>
+      <div className="heading heading--2xl">Heading (2xl)</div>
+      <div className="heading heading--xl">Heading (xl)</div>
+      <div className="heading heading--lg">Heading (lg)</div>
+      <div className="heading heading--md">Heading (md)</div>
+      <div className="heading heading--sm">Heading (sm)</div>
+      <div className="heading heading--xs">Heading (xs)</div>
+      <div className="heading heading--2xs">Heading (2xs)</div>
+    </div>
+  )
+
+export const BodyComposition = (props: Props) => (
+    <div {...props}>
+      <div className="body body--3xl">Body (3xl)</div>
+      <div className="body body--2xl">Body (2xl)</div>
+      <div className="body body--xl">Body (xl)</div>
+      <div className="body body--lg">Body (lg)</div>
+      <div className="body body--md">Body (md)</div>
+      <div className="body body--sm">Body (sm)</div>
+      <div className="body body--xs">Body (xs)</div>
+    </div>
+  )
+
+export const DetailComposition = (props: Props) => (
+    <div {...props}>
+      <div className="detail detail--xl">Detail (xl)</div>
+      <div className="detail detail--lg">Detail (lg)</div>
+      <div className="detail detail--md">Detail (md)</div>
+      <div className="detail detail--sm">Detail (sm)</div>
+    </div>
+  )
+
+
+export const CodeComposition = (props: Props) => (
+    <div {...props}>
+      <div className="code code--xl">Code (xl)</div>
+      <div className="code code--lg">Code (lg)</div>
+      <div className="code code--md">Code (md)</div>
+      <div className="code code--sm">Code (sm)</div>
+      <div className="code code--xs">Code (xs)</div>
+    </div>
+  )
--- apps/docs/stories/theme/typography/typography.stories.tsx
+++ apps/docs/stories/theme/typography/typography.stories.tsx
@@ -1,65 +1,37 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { TypographyComposition } from './typography.composition';
+import { BodyComposition, CodeComposition, DetailComposition, HeadingComposition, Props, TypographyComposition } from './typography.composition';
 
 const meta: Meta<typeof TypographyComposition> = {
   title: 'apsphysics.prism-ui/theme/typography',
-  component: TypographyComposition,
+  component: (props: Props) => <div {...props}></div>,
+  // args: { className: 'text-gray-500' }
+};
+
+export const Typography: Story = {
+  render: TypographyComposition,
+  args: {}
 };
 
 export default meta;
 type Story = StoryObj<typeof TypographyComposition>;
 
 export const Heading: Story = {
-  render: () => (
-    <>
-      <div className="heading heading--3xl">Heading (3xl)</div>
-      <div className="heading heading--2xl">Heading (2xl)</div>
-      <div className="heading heading--xl">Heading (xl)</div>
-      <div className="heading heading--lg">Heading (lg)</div>
-      <div className="heading heading--md">Heading (md)</div>
-      <div className="heading heading--sm">Heading (sm)</div>
-      <div className="heading heading--xs">Heading (xs)</div>
-      <div className="heading heading--2xs">Heading (2xs)</div>
-    </>
-  ),
+  render: HeadingComposition,
+  args: {}
 };
 
 export const Body: Story = {
-  render: () => (
-    <>
-      <div className="body body--3xl">Body (3xl)</div>
-      <div className="body body--2xl">Body (2xl)</div>
-      <div className="body body--xl">Body (xl)</div>
-      <div className="body body--lg">Body (lg)</div>
-      <div className="body body--md">Body (md)</div>
-      <div className="body body--sm">Body (sm)</div>
-      <div className="body body--xs">Body (xs)</div>
-    </>
-  ),
+  render: BodyComposition,
+  args: {}
 };
 
 export const Detail: Story = {
-  render: () => (
-    <>
-      <div className="detail detail--xl">Detail (xl)</div>
-      <div className="detail detail--lg">Detail (lg)</div>
-      <div className="detail detail--md">Detail (md)</div>
-      <div className="detail detail--sm">Detail (sm)</div>
-    </>
-  ),
+  render: DetailComposition,
+  args: {}
 };
 
 export const Code: Story = {
-  render: () => (
-    <>
-      <div className="code code--xl">Code (xl)</div>
-      <div className="code code--lg">Code (lg)</div>
-      <div className="code code--md">Code (md)</div>
-      <div className="code code--sm">Code (sm)</div>
-      <div className="code code--xs">Code (xs)</div>
-    </>
-  ),
+  render: CodeComposition,
+  args: {}
 };
-
-export const Composition: Story = {};
